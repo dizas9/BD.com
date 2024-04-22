064146a0228e955a2fd97bb80de5323cf7756593
dizas9 <sazid.ice07@gmail.com>
2024-04-22 15:04:25 +0600

    work with sanity, UI

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,61 @@
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { Button } from "@/components/ui/button";
+import { ShoppingBasket } from "lucide-react";
+
+
+const link = [
+  { name: "Home", href: "/" },
+  { name: "Men", href: "/Men" },
+  { name: "Women", href: "/Women" },
+  { name: "Teen", href: "/Teen" },
+];
+
+export default function Navbar() {
+  const pathname = usePathname();
+  return (
+    <header className="mb-8 border-b">
+      <div className="flex items-center justify-between mx-auto max-w-2xl px-4 sm:px-6 lg:max-w-7xl">
+        <Link href={"/"}>
+          <h1 className="text-2xl md:text-4xl font-bold">
+            BD <span className="text-primary">.com</span>
+          </h1>
+        </Link>
+
+        {/* navlinks */}
+
+        <nav className="hidden gap-12 lg:flex 2xl:ml-16">
+          {link.map((link, index) => (
+            <div key={index}>
+              {pathname === link.href ? (
+                <Link
+                  href={link.href}
+                  className="text-lg font-semibold text-primary"
+                >
+                  {link.name}
+                </Link>
+              ) : (
+                <Link
+                  href={link.href}
+                  className="text-lg font-semibold text-gray-600 transition duration-100 hover:text-primary"
+                >
+                  {link.name}
+                </Link>
+              )}
+            </div>
+          ))}
+        </nav>
+
+        {/* cart Button */}
+
+        <div className="flex divide-x border-r sm:border-l">
+          <Button variant="outline" className="flex flex-col gap-y-1.5 h-12 w-12 sm:w-20 md:h-24 rounded-none">
+            <ShoppingBasket/>
+            <span className="hidden text-xs font-semibold text-gray-500 sm:block">Cart</span>
+          </Button>
+        </div>
+      </div>
+    </header>
+  );
+}
